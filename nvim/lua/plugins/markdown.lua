aa8b35330ce16c4cccfd4a4c5e23c1013601f223
Guaidaodl <linyubinas@gmail.com>
2026-05-07 18:32:05 +0800

    feat(vim): 增加 markdown preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -7,4 +7,17 @@ return {
             vim.g.vim_markdown_folding_disabled = 1
         end,
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        build = "cd app && npx --yes yarn install",
+        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
+        keys = {
+            { "<leader>fp", "<cmd>MarkdownPreviewToggle<CR>", ft = "markdown", desc = "markdown preview" },
+        },
+        init = function()
+            vim.g.mkdp_auto_close = 0
+            vim.g.mkdp_theme = "dark"
+        end,
+    },
 }
